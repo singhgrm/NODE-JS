ca8766361b152e2c2f8eb92c925f28115720be39
singhgrm <102731790+singhgrm@users.noreply.github.com>
2022-11-24 12:15:09 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-//here we will learn how to create synchronous file 
+//here we are going to create file using asynchronous methos..
+//it work simultanisily..as cpu work..but at one time work on only one file
+const fs=require('fs');
+fs.writeFile("read.txt",'hii..grm ,hope u are doing well...',(err)=>{
+    console.log("now completed");
+//id there is aeeroe in file it will show that error..if no error it will print null
+    console.log(err);
+});
+//err argument is used to check error
+fs.appendFile('read.txt',"   adding some extra information..",(err)=>
+{
+    //it can be blank or we can print anything..
+});
 
-//the best thing of nodejs we can create file, FileSystem
-const fs=require("fs");
-const { fileURLToPath } = require("url");
-//asynchoronus means priority based synchronods means first come first serve
-fs.writeFileSync("read.txt","welcome  to learn node js");
-//it will override the data on fs.read.txt
-fs.writeFileSync("read.txt","welcome  singh grm, to learn node js");
-//we can add some extra information in same file using append..
-fs.appendFileSync("read.txt" ," hey there whatbare you learning..now..");
-//nodejs have additional data types buffer..
-//buffer is used to store binary data..
-const bff_data=fs.readFileSync("read.txt");
-//console.log(bff_data);
-//bff_data.toString(); it will give original data instead of buffer
-org_data=bff_data.toString();
-console.log(org_data);
-//we can rename the file..name
-fs.renameSync("read.txt","read_write.txt");
+//now read the data
+fs.readFile('read.txt','UTF-8',(err,data)=>
+{
+    
+console.log(data);
+});
+
 
